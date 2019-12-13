4daab08293a676320e933ad9fff5cf5a126f63d5
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-13 14:04:32 +0000

    created ParkBox container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
+import ParkBox from './containers/ParkBox'
 
 function App() {
   return (
+    <ParkBox />
   );
 }
 
---
+++ src/containers/ParkBox.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class ParkBox extends Component {
+
+  render(){
+    return(
+      <p>This is the ParkBox</p>
+    )
+  }
+}
+
+export default ParkBox

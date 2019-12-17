5f2ab0a518802c8fa2f2eea6de18d05b042b1948
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-17 11:52:18 +0000

    added timeout to feed button

--- src/components/FeedButton.js
+++ src/components/FeedButton.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const FeedButton = (props) => {
+class FeedButton extends Component{
 
+  constructor(props){
+    super(props)
+    this.state ={
+      showBoolean: true
+    }
+
+    this.toggleShowBoolean = this.toggleShowBoolean.bind(this)
+    this.buttonDisappear = this.buttonDisappear.bind(this)
+  }
+
+  toggleShowBoolean(){
+    this.state.showBoolean ? this.setState({showBoolean: false}) : this.setState({showBoolean: true})
+  }
+
+  buttonDisappear(){
+    this.toggleShowBoolean()
+    setTimeout(()=>this.toggleShowBoolean(), 30000)
+  }
+
+render(){
+  if(this.state.showBoolean){
   return(
-    <button>Feed me</button>
+    <button onClick={this.buttonDisappear}>Feed me!</button>
   )
+}
+return(
+  <p>Fed</p>
+)
+}
 
 }
 
